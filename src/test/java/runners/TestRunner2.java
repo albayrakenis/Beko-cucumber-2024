df9599e5aca522fbca3376b9c923317abc822ea3
Enis <1albayrakenis@gmail.com>
2025-04-17 00:01:43 +0300

    mail

--- src/test/java/runners/TestRunner2.java
+++ src/test/java/runners/TestRunner2.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 
 
 @CucumberOptions(
-        tags = "@smoke",
+        tags = "@loginSuccess",
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json",
