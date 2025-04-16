54bcfa5eef225d514a0fd68cc335c82915707a95
Enis <1albayrakenis@gmail.com>
2025-04-17 00:12:05 +0300

    Deneme commit'i - GitHub Actions test

--- src/test/java/runners/TestRunner2.java
+++ src/test/java/runners/TestRunner2.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 
 
 @CucumberOptions(
-        tags = "@loginSuccess",
+        tags = "@yanlissifre",
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json",
