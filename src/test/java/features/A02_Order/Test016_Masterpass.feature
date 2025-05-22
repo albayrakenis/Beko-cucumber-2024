668f18be8d9c62dd0fa9d564f5734238bf0bbf77
Enis ALBAYRAK <1albayrakenis@gmail.com>
2025-05-22 09:14:22 +0300

    order

--- src/test/java/features/A02_Order/Test016_Masterpass.feature
+++ src/test/java/features/A02_Order/Test016_Masterpass.feature
@@ -1,7 +1,7 @@
 Feature: Satin Alma Islemleri
 
 
-     @order @masterpass @smoke
+  @masterpass
   Scenario Outline: Test016- Masterpass
     When I see "HomePage" page
     When I go to "<url>" with configuration
--- src/test/java/runners/TestRunner2.java
+++ src/test/java/runners/TestRunner2.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 
 
 @CucumberOptions(
-        tags = "@smoke",
+        tags = "@order",
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json",
